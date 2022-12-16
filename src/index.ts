2ea4b51459c9cb89475c7c6102409bd5278d10f3
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-12-16 16:09:26 -0300

    refactor: remove fixed strings

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,16 @@ import cron from 'node-cron'
 import execCmd from './helpers/execCmd';
 import 'dotenv/config'
 
+const JOBS = {
+  WESTERN_UNION: {
+    cron: '* */30 * * * *',
+    cmd: `source ${__dirname}/scripts/fetch_data.sh | grep -o '"fx_rate":[[:digit:]]....'`
+  }
+}
+
+console.log(`Server is running and has currently ${Object.keys(JOBS).length} jobs running.`)
+
 /* Running the fetch_data.sh script every 30 minutes. */
-cron.schedule('*/30 * * * *', async () => {
-  execCmd(`source ${__dirname}/scripts/fetch_data.sh | grep -o '"fx_rate":[[:digit:]]....'`)
+cron.schedule(JOBS.WESTERN_UNION.cron, async () => {
+  execCmd(JOBS.WESTERN_UNION.cmd)
 })
