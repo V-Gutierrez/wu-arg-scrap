d341339b2496d7db11f6ac6e10dde1bf0bee8e76
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-12-19 18:24:56 -0300

    feat: create data analysis setup

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,10 @@ WORKDIR /usr/app
 
 COPY ./package*.json ./
 
-RUN apk add g++ make py3-pip curl && npm install && rm -rf /var/cache/apk/*
+RUN apk add g++ make jq py3-configobj py3-pip py3-setuptools python3 python3-dev curl \
+  && pip3 install pathlib matplotlib pandas \
+  && npm install \
+  && rm -rf /var/cache/apk/*
 
 COPY . .
 
---
+++ src/data_scripts/plot_csv_charts.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# python3 -u "src/data_scripts/plot_csv_charts.py" "BRL_$TARGET_CURRENCY" "$TARGET_CURRENCY"
+
+from pathlib import Path
+import matplotlib.pyplot as plt
+import sys
+import pandas as pd
+
+currency_pair = sys.argv[1]
+target_currency = sys.argv[2]
+file_path = p = Path(f"src/databases/{target_currency}.csv").resolve()
+
+# Read the data from the CSV file
+
+columns = ["BASE_BRL", f"PRICE_{target_currency}", "DATE"]
+
+df = pd.read_csv(file_path, delimiter=";", usecols=columns)
+
+# Add axis labels and subplot labels
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax.set(
+    xlabel="Date",
+    ylabel=f"Exchange rate to 1 BRL",
+    title=f"Exchange rate of {currency_pair}"
+)
+
+plt.setp(ax.get_xticklabels(), rotation=90)
+
+# Create chart and save the plot
+ax.plot(df["DATE"], df[f"PRICE_{target_currency}"])
+plt.savefig(
+    f"src/data_outputs/{currency_pair}_chart.png",
+    dpi=300,
+    bbox_inches="tight"
+)
