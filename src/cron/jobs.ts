1e1535c6f98427c434905a2ceef38db9c6e78b84
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-01-04 10:20:20 -0400

    fix: restore default cron timings on jobs

--- src/cron/jobs.ts
+++ src/cron/jobs.ts
@@ -1,13 +1,13 @@
 export const JOBS = [
   {
     jobIdentifier: 'WESTERN_UNION_BRL_ARS',
-    cron: '*/47 8-20 * * *',
-    cmd: `source ${__dirname}/scripts/fetching/BRL_ARS.sh --target WU-BRL-ARS`
+    cron: '*/60 8-20 * * *',
+    cmd: `source src/scripts/fetching/BRL_ARS.sh --target WU-BRL-ARS`
   },
   {
     jobIdentifier: 'WESTERN_UNION_BRL_USD',
-    cron: '*/47 8-20 * * *',
-    cmd: `source ${__dirname}/scripts/fetching/BRL_USD.sh --target WU-BRL-USD`
+    cron: '*/60 8-20 * * *',
+    cmd: `source src/scripts/fetching/BRL_USD.sh --target WU-BRL-USD`
   },
   {
     jobIdentifier: 'HEALTH_CHECK',
