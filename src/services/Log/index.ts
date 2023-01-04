62334b1de31a903b2d0cf23f900c6097555cd32e
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-01-04 10:20:53 -0400

    feat: implement verbosity levels

--- src/services/Log/index.ts
+++ src/services/Log/index.ts
@@ -5,12 +5,28 @@ class Log {
     return new Date(Date.now())
   }
 
-   info<T>(load: T) {
-    console.info(`[${this.generateTimestamp()}]`, load)
+  info<T>(message: T) {
+    console.info(`[INFO] [${this.generateTimestamp()}]`, message)
   }
 
-   error<T>(message: string, load: T) {
-    console.error(message, toJSON(load))
+  error<T>(message: string, load: T) {
+    console.error(`[ERROR] [${this.generateTimestamp()}] ${message}`, toJSON(load))
+  }
+
+  fatal<T>(message: string, load: T) {
+    console.error(`[FATAL] [${this.generateTimestamp()}] ${message}`, toJSON(load))
+  }
+
+  trace(message: string) {
+    console.info(`[TRACE] [${this.generateTimestamp()}] ${message}`)
+  }
+
+  warn(message: string) {
+    console.warn(`[WARNING] [${this.generateTimestamp()}] ${message}`)
+  }
+
+  debug(message: string) {
+    console.log(`[DEBUG] [${this.generateTimestamp()}] ${message}`)
   }
 }
 
